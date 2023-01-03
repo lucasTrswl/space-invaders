ade8b1f8c327bd59c05a210c9dde259d02b54035
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-01-03 14:17:24 +0100

    vaisseau move

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var ligne1 = document.querySelector(".ligne1");
 var ligne2 = document.querySelector(".ligne2");
 var ligne3 = document.querySelector(".ligne3");
 
+var board = document.querySelector('.board');
+var board_coord = board.getBoundingClientRect();
+
+var vaisseau = document.querySelector('.vaisseau');
+var initial_vaisseau_coord =vaisseau.getBoundingClientRect();
+var vaisseau_coord = initial_vaisseau_coord;
+var vaisseau_common = 
+    document.querySelector('.vaisseau').getBoundingClientRect();
+
 
 bad = []
 var element = { name: "bad", image: "images/ennemies.png" }
@@ -17,4 +26,37 @@ function gamestart(){
 }
 
 gamestart();
+
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'ArrowLeft') {
+
+        // Pour ne pas que ça bouge la fenêtre avec
+        e.preventDefault();
+
+        // Permet de monter le paddle 2
+        vaisseau.style.left = (Math.max(
+            board_coord.left,
+            vaisseau_coord.left - window.innerHeight * 0.1
+        )+50) + 'px';
+        
+
+        // Change les coordonnées du paddle vu que l'on vient de le bouger
+        vaisseau_coord = vaisseau.getBoundingClientRect();
+    }
+    if (e.key == 'ArrowRight') {
+
+        // Pour ne pas que ça bouge la fenêtre avec
+        e.preventDefault();
+        // Permet de descendre le paddle 2
+
+        vaisseau.style.left =
+        (Math.min(
+            board_coord.right - vaisseau_common.height,
+            vaisseau_coord.left + window.innerHeight * 0.1
+        )-50) + 'px';
+
+        // Change les coordonnées du paddle vu que l'on vient de le bouger
+        vaisseau_coord = vaisseau.getBoundingClientRect();
+    }
+});
 
