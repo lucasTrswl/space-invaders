9ccbe55a3c1201e5194ab90210859e139d237c82
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-01-04 14:24:33 +0100

    new

--- app.js
+++ app.js
@@ -1,62 +1,133 @@
-var ligne1 = document.querySelector(".ligne1");
-var ligne2 = document.querySelector(".ligne2");
-var ligne3 = document.querySelector(".ligne3");
+const grid = document.querySelector('.grid')
+const resultsDisplay = document.querySelector('.results')
+let currentShooterIndex = 202
+let width = 15
+let direction = 1
+let invadersId
+let goingRight = true
+let aliensRemoved = []
+let results = 0
 
-var board = document.querySelector('.board');
-var board_coord = board.getBoundingClientRect();
-
-var vaisseau = document.querySelector('.vaisseau');
-var initial_vaisseau_coord =vaisseau.getBoundingClientRect();
-var vaisseau_coord = initial_vaisseau_coord;
-var vaisseau_common = 
-    document.querySelector('.vaisseau').getBoundingClientRect();
-
-
-bad = []
-var element = { name: "bad", image: "images/ennemies.png" }
-
-function gamestart(){
-    tab = [ligne1, ligne2, ligne3];
-        for (let i in tab) {
-            for (let j = 0; j < 12; j++) {
-                tab[i].innerHTML += `
-                <div class="grille"> <img src="images/ennemies.png"> </div> `;
-            }
-        }
+for (let i = 0; i < 225; i++) {
+  const square = document.createElement('div')
+  grid.appendChild(square)
 }
 
-gamestart();
+const squares = Array.from(document.querySelectorAll('.grid div'))
 
-document.addEventListener('keydown', (e) => {
-    if (e.key == 'ArrowLeft') {
+const alienInvaders = [
+  0,1,2,3,4,5,6,7,8,9,10,11,
+  15,16,17,18,19,20,21,22,23,24,25,26,
+  30,31,32,33,34,35,36,37,38,39,40,41
+]
 
-        // Pour ne pas que ça bouge la fenêtre avec
-        e.preventDefault();
-
-        // Permet de monter le paddle 2
-        vaisseau.style.left = (Math.max(
-            board_coord.left,
-            vaisseau_coord.left - window.innerHeight * 0.1
-        )+50) + 'px';
-        
-
-        // Change les coordonnées du paddle vu que l'on vient de le bouger
-        vaisseau_coord = vaisseau.getBoundingClientRect();
+function draw() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    if(!aliensRemoved.includes(i)) {
+      squares[alienInvaders[i]].classList.add('invader')
     }
-    if (e.key == 'ArrowRight') {
+  }
+}
 
-        // Pour ne pas que ça bouge la fenêtre avec
-        e.preventDefault();
-        // Permet de descendre le paddle 2
+draw()
 
-        vaisseau.style.left =
-        (Math.min(
-            board_coord.right - vaisseau_common.height,
-            vaisseau_coord.left + window.innerHeight * 0.1
-        )-50) + 'px';
+function remove() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    squares[alienInvaders[i]].classList.remove('invader')
+  }
+}
 
-        // Change les coordonnées du paddle vu que l'on vient de le bouger
-        vaisseau_coord = vaisseau.getBoundingClientRect();
+squares[currentShooterIndex].classList.add('shooter')
+
+
+function moveShooter(e) {
+  squares[currentShooterIndex].classList.remove('shooter')
+  switch(e.key) {
+    case 'ArrowLeft':
+      if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+      break
+    case 'ArrowRight' :
+      if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+      break
+  }
+  squares[currentShooterIndex].classList.add('shooter')
+}
+document.addEventListener('keydown', moveShooter)
+
+function moveInvaders() {
+  const leftEdge = alienInvaders[0] % width === 0
+  const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width -1
+  remove()
+
+  if (rightEdge && goingRight) {
+    for (let i = 0; i < alienInvaders.length; i++) {
+      alienInvaders[i] += width +1
+      direction = -1
+      goingRight = false
     }
-});
+  }
 
+  if(leftEdge && !goingRight) {
+    for (let i = 0; i < alienInvaders.length; i++) {
+      alienInvaders[i] += width -1
+      direction = 1
+      goingRight = true
+    }
+  }
+
+  for (let i = 0; i < alienInvaders.length; i++) {
+    alienInvaders[i] += direction
+  }
+
+  draw()
+
+  if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+    resultsDisplay.innerHTML = 'GAME OVER'
+    clearInterval(invadersId)
+  }
+
+  for (let i = 0; i < alienInvaders.length; i++) {
+    if(alienInvaders[i] > (squares.length)) {
+      resultsDisplay.innerHTML = 'GAME OVER'
+      clearInterval(invadersId)
+    }
+  }
+  if (aliensRemoved.length === alienInvaders.length) {
+    resultsDisplay.innerHTML = 'YOU WIN'
+    clearInterval(invadersId)
+  }
+}
+invadersId = setInterval(moveInvaders, 600)
+
+function shoot(e) {
+  let laserId
+  let currentLaserIndex = currentShooterIndex
+  function moveLaser() {
+    squares[currentLaserIndex].classList.remove('laser')
+    currentLaserIndex -= width
+    squares[currentLaserIndex].classList.add('laser')
+
+    if (squares[currentLaserIndex].classList.contains('invader')) {
+      squares[currentLaserIndex].classList.remove('laser')
+      squares[currentLaserIndex].classList.remove('invader')
+      squares[currentLaserIndex].classList.add('boom')
+
+      setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+      clearInterval(laserId)
+
+      const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+      aliensRemoved.push(alienRemoved)
+      results++
+      resultsDisplay.innerHTML = results
+      console.log(aliensRemoved)
+
+    }
+
+  }
+  switch(e.key) {
+    case ' ':
+      laserId = setInterval(moveLaser, 100)
+  }
+}
+
+document.addEventListener('keydown', shoot)
