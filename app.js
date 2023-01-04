50762cb70decfbf92f9a5f28599772a8811ca4e6
nom2 <nom2@email.com>
2023-01-04 09:23:31 +0100

    Position alien + joueur

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ for(let i = 0; i < 255; i++){
 }
 // cadres des divs
 const cadres = document.querySelectorAll('#grille div');
+// position des aliens dans le cadres
+const alien = [
+    1,2,3,4,5,6,7,8,9,
+    16,17,18,19,20,21,22,23,24,
+    31,32,33,34,35,36,37,38,39
+];
+
+//positon du tireur dans la cadre
+let postionTireur = 232;
+cadres[postionTireur].classList.add('tireur');
+//postion des aliens dans la cadre
+alien.forEach(alien => cadres[alien].classList.add('alien'));
+
+
